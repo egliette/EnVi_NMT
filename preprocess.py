bba6ce5fdbad054c75fd55137f8d314ec7cba34e
Minh Tú Lê <leminhtu74@gmail.com>
2023-07-07 11:55:16 +0700

    train vocab before mapping

--- preprocess.py
+++ preprocess.py
@@ -18,6 +18,12 @@ def load_dataloader_from_fpath(pair_fpath, src_tok, tgt_tok, batch_size, max_len
                                                                                 src_tok.tokenize,
                                                                                 tgt_tok.tokenize)
 
+    if is_train:
+        print(f"Create vocabulary from {pair_fpath['src']}...")
+        src_tok.train_vocab(src_tok_sents, is_tokenized=True)
+        print(f"Create vocabulary from {pair_fpath['tgt']}...")
+        tgt_tok.train_vocab(tgt_tok_sents, is_tokenized=True)
+
     dataset = ParallelDataset(src_tok_sents, tgt_tok_sents, src_tok, tgt_tok, is_sorted=True)
     parallel_vocab = ParallelVocabulary(src_tok.vocab, tgt_tok.vocab, device)
 
@@ -27,10 +33,6 @@ def load_dataloader_from_fpath(pair_fpath, src_tok, tgt_tok, batch_size, max_len
                         shuffle=False)
 
     if is_train:
-        print(f"Create vocabulary from {pair_fpath['src']}...")
-        src_tok.train_vocab(src_tok_sents, is_tokenized=True)
-        print(f"Create vocabulary from {pair_fpath['tgt']}...")
-        tgt_tok.train_vocab(tgt_tok_sents, is_tokenized=True)
         return loader, src_tok, tgt_tok
     else:
         return loader

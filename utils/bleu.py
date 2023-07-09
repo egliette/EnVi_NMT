97096c4f591773f3b431ccbd03e926804ba48373
Minh Tú Lê <leminhtu74@gmail.com>
2023-07-09 20:48:21 +0700

    update BLEU

--- utils/bleu.py
+++ utils/bleu.py
@@ -5,28 +5,32 @@ from collections import Counter
 def count_n_gram(tokens, n):
     return Counter([tuple(tokens[i:i + n]) for i in range(len(tokens) + 1 - n)])
 
-def precision_i(pred_tokens, tgt_tokens, i):
-    pred_i_grams = count_n_gram(pred_tokens, i)
-    tgt_i_grams = count_n_gram(tgt_tokens, i)
-
+def precision_i(pred_sents, tgt_sents, i):
     total_matched_i_grams = 0
     total_pred_i_grams = 0
-    for pred_i_gram, pred_count in pred_i_grams.items():
-        tgt_matched_count = tgt_i_grams.get(pred_i_gram, 0)
-        total_matched_i_grams += min(tgt_matched_count, pred_count)
-        total_pred_i_grams += pred_count
-    
-    if total_pred_i_grams == 0:
-        return 0
-    else:
-        return total_matched_i_grams / total_pred_i_grams
+    for pred_tokens, tgt_tokens in zip(pred_sents, tgt_sents):
+        pred_i_grams = count_n_gram(pred_tokens, i)
+        tgt_i_grams = count_n_gram(tgt_tokens, i)
 
-def calculate_bleu(pred_tokens, tgt_tokens):
-    brevity_penalty = min(1, math.exp(1 - len(tgt_tokens)/len(pred_tokens)))
-    n_gram_overlap = 1
+        total_pred_i_grams += len(pred_tokens) - i + 1
+        for pred_i_gram, pred_count in pred_i_grams.items():
+            tgt_matched_count = tgt_i_grams.get(pred_i_gram, 0)
+            total_matched_i_grams += min(tgt_matched_count, pred_count)
+
+    return total_matched_i_grams / total_pred_i_grams
+
+def calculate_bleu(pred_sents, tgt_sents):
+    n_gram_overlap = 0
 
     for i in range(1, 5):
-        n_gram_overlap *= precision_i(pred_tokens, tgt_tokens, i)
-    
-    bleu = brevity_penalty * math.pow(n_gram_overlap, 1/4)
+        n_gram_overlap += 0.25 * math.log(precision_i(pred_sents,  tgt_sents, i))
+
+    geometric_mean = math.exp(n_gram_overlap)
+
+    r = sum(len(tgt_tokens) for tgt_tokens in pred_sents)
+    c = sum(len(pred_tokens) for pred_tokens in pred_sents)
+        
+    brevity_penalty = math.exp(min(1 - r/c, 0))
+
+    bleu = brevity_penalty * geometric_mean
     return bleu
